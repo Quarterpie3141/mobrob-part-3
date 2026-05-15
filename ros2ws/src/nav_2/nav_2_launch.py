c87e8a7b897ed72e2d20d4941e57c78da470332a
itzmeh <itzmehahaha04@gmail.com>
2026-05-15 14:57:17 +0800

    no collision monitor

--- ros2ws/src/nav_2/nav_2_launch.py
+++ ros2ws/src/nav_2/nav_2_launch.py
@@ -27,7 +27,8 @@ def generate_launch_description():
         launch_arguments={
             'use_sim_time': 'false', # Setting to false for your physical robot
             'params_file': LaunchConfiguration('params_file'),
-            'autostart': 'true'
+            'autostart': 'true',
+            'use_collision_monitor': 'false',
         }.items()
     )
     # This node bypasses the collision_monitor by manually bridging the topics
