da203953891d2ca1d6226c619d7edf91377b0bf4
narin <pioneer>
2026-05-22 04:09:10 +0800

    asdsa

--- ros2ws/src/estop/setup.py
+++ ros2ws/src/estop/setup.py
@@ -1,36 +1,29 @@
 import os
-
 from setuptools import find_packages, setup
 
 package_name = 'estop'
 
 setup(
     name=package_name,
-    share_path = "share/" + package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-                (
-            os.path.join(share_path, "launch"),
-            [os.path.join("launch", "moving_tracker.launch.py")],
-        ),
+        ('share/' + package_name, ['package.xml']),         
+        (os.path.join('share', package_name, 'launch'), ['launch/moving_tracker.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='mehar',
     maintainer_email='itzmehahaha04@gmail.com',
-    description='TODO: Package description',
+    description='LiDAR moving object tracking node',
     license='Apache-2.0',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    tests_require=['pytest'],
     entry_points={
-        'console_scripts': [ 'moving_tracker = tracker_pkg.moving_tracker:main',
+        'console_scripts': [ 
+            # Format: 'executable_target = python_package_folder.filename:main'
+            'moving_object_tracker = estop.moving_object_tracker:main',
         ],
     },
 )
