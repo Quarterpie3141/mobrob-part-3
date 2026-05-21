bd6cc7da3c731ea10828ab54a273a396e7cfc4d0
Prashan Wijesinghe <86412584+Quarterpie3141@users.noreply.github.com>
2026-05-21 19:05:50 +0800

    Update apollo.py

--- ros2ws/src/global_controller/global_controller/apollo.py
+++ ros2ws/src/global_controller/global_controller/apollo.py
@@ -22,20 +22,20 @@ import numpy as np
 import math
 import json
 
+ HARDCODED_WAYPOINTS: List[Tuple[float, float, float]] = [
+     (4.0, 0.0, 0.0), 
+     (8.0, 0.0, 3.141), 
+     (0.0, 0.0, 3.141),
+     (-4.0, 0.0, 0.0), 
+     (0.0, 0.0, 0.0)
+ ]
+
 # HARDCODED_WAYPOINTS: List[Tuple[float, float, float]] = [
-#     (4.0, 0.0, 0.0), 
-#     (8.0, 0.0, 3.141), 
+#     (4.0, 0.0, 3.141), 
 #     (0.0, 0.0, 3.141),
 #     (-4.0, 0.0, 0.0), 
 #     (0.0, 0.0, 0.0)
 # ]
-
-HARDCODED_WAYPOINTS: List[Tuple[float, float, float]] = [
-    (4.0, 0.0, 3.141), 
-    (0.0, 0.0, 3.141),
-    (-4.0, 0.0, 0.0), 
-    (0.0, 0.0, 0.0)
-]
 
 CLASSIFIED_WAYPOINTS: List[Tuple[float, float, float, str]] = [
 ]
@@ -520,8 +520,8 @@ class GlobalControllerNode(Node):
                 cx_m = centroids[i][0] * res + costmap.info.origin.position.x
                 cy_m = centroids[i][1] * res + costmap.info.origin.position.y
 
-                #if (cx_m < 10 and cx_m > -6) and (cy_m < 4 and cy_m > -8) and math.sqrt(cx_m**2 + cy_m**2) < 11.0:                    
-                if (cx_m < 4 and cx_m > -4) and (cy_m < 4 and cy_m > -4) and math.sqrt(cx_m**2 + cy_m**2) < 11.0:                    
+                if (cx_m < 10 and cx_m > -6) and (cy_m < 4 and cy_m > -8) and math.sqrt(cx_m**2 + cy_m**2) < 11.0:                    
+                #if (cx_m < 4 and cx_m > -4) and (cy_m < 4 and cy_m > -4) and math.sqrt(cx_m**2 + cy_m**2) < 11.0:                    
                     if len(object_positions) == 0:
                         object_positions.append((cx_m, cy_m, 0.0))
                     else: 
